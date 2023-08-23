77a5fea91bea15ac44e094bcb6230ca4b0b04654
Dan Chen <dan.chen@boardware.com>
2023-08-23 10:03:54 +0800

    feat

--- argusapi.go
+++ argusapi.go
@@ -6,46 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Monitor struct {
-	Status               MonitorStatus  `json:"status"`
-	Retries              int64          `json:"retries"`
-	AcceptedStatusCodes  *[]Pair        `json:"acceptedStatusCodes,omitempty"`
-	Interval             int64          `json:"interval"`
-	Timeout              int64          `json:"timeout"`
-	Url                  string         `json:"url"`
-	Id                   string         `json:"id"`
-	Type                 MonitorType    `json:"type"`
-	Notifications        []Notification `json:"notifications"`
-	Description          string         `json:"description"`
-	Method               *HttpMethod    `json:"method,omitempty"`
-	NotificationInterval int64          `json:"notificationInterval"`
-	Name                 string         `json:"name"`
-	Headers              *[]Pair        `json:"headers,omitempty"`
+type MonitoringRecordList struct {
+	Data       []MonitoringRecord `json:"data"`
+	Pagination Pagination         `json:"pagination"`
 }
-type MonitorList struct {
-	Data       []Monitor  `json:"data"`
-	Pagination Pagination `json:"pagination"`
+type EmailReceivers struct {
+	To  []string `json:"to"`
+	Cc  []string `json:"cc"`
+	Bcc []string `json:"bcc"`
 }
 type Report struct {
 	Cron           *string         `json:"cron,omitempty"`
 	Period         *int64          `json:"period,omitempty"`
 	EmailReceivers *EmailReceivers `json:"emailReceivers,omitempty"`
 }
-type MonitorProperties struct {
-	Type                 MonitorType    `json:"type"`
-	Timeout              int64          `json:"timeout"`
-	Retries              int64          `json:"retries"`
-	Status               MonitorStatus  `json:"status"`
-	AcceptedStatusCodes  *[]Pair        `json:"acceptedStatusCodes,omitempty"`
-	NotificationInterval int64          `json:"notificationInterval"`
-	Reports              []Report       `json:"reports"`
-	Name                 string         `json:"name"`
-	Description          string         `json:"description"`
-	Interval             int64          `json:"interval"`
-	Url                  string         `json:"url"`
-	Method               *HttpMethod    `json:"method,omitempty"`
-	Headers              []Pair         `json:"headers"`
-	Notifications        []Notification `json:"notifications"`
+type Pagination struct {
+	Index int64 `json:"index"`
+	Limit int64 `json:"limit"`
+	Total int64 `json:"total"`
 }
 type MonitoringRecord struct {
 	Id           string           `json:"id"`
@@ -55,48 +33,67 @@ type MonitoringRecord struct {
 	ResponseTime *int64           `json:"responseTime,omitempty"`
 	Result       MonitoringResult `json:"result"`
 }
-type MonitoringRecordList struct {
-	Data       []MonitoringRecord `json:"data"`
-	Pagination Pagination         `json:"pagination"`
-}
-type EmailReceivers struct {
-	Cc  []string `json:"cc"`
-	Bcc []string `json:"bcc"`
-	To  []string `json:"to"`
-}
-type Pair struct {
-	Left  string `json:"left"`
-	Right string `json:"right"`
+type PutMonitorRequest struct {
+	Headers              *[]Pair        `json:"headers,omitempty"`
+	NotificationInterval int64          `json:"notificationInterval"`
+	Body                 *string        `json:"body,omitempty"`
+	Interval             int64          `json:"interval"`
+	Description          string         `json:"description"`
+	Type                 MonitorType    `json:"type"`
+	Retries              int64          `json:"retries"`
+	AcceptedStatusCodes  *[]Pair        `json:"acceptedStatusCodes,omitempty"`
+	Notifications        []Notification `json:"notifications"`
+	Name                 string         `json:"name"`
+	Timeout              int64          `json:"timeout"`
+	Url                  string         `json:"url"`
+	Method               *HttpMethod    `json:"method,omitempty"`
+	Status               MonitorStatus  `json:"status"`
 }
 type Notification struct {
 	Type           NotificationType `json:"type"`
 	EmailReceivers *EmailReceivers  `json:"emailReceivers,omitempty"`
 }
-type PutMonitorRequest struct {
-	Description          string         `json:"description"`
+type Pair struct {
+	Left  string `json:"left"`
+	Right string `json:"right"`
+}
+type MonitorProperties struct {
 	Interval             int64          `json:"interval"`
-	Timeout              int64          `json:"timeout"`
 	Status               MonitorStatus  `json:"status"`
 	Url                  string         `json:"url"`
+	Notifications        []Notification `json:"notifications"`
+	Description          string         `json:"description"`
+	Type                 MonitorType    `json:"type"`
+	Headers              []Pair         `json:"headers"`
+	NotificationInterval int64          `json:"notificationInterval"`
+	Reports              []Report       `json:"reports"`
+	Name                 string         `json:"name"`
+	Timeout              int64          `json:"timeout"`
 	Retries              int64          `json:"retries"`
 	Method               *HttpMethod    `json:"method,omitempty"`
 	AcceptedStatusCodes  *[]Pair        `json:"acceptedStatusCodes,omitempty"`
+}
+type Monitor struct {
+	Description          string         `json:"description"`
+	Url                  string         `json:"url"`
+	Method               *HttpMethod    `json:"method,omitempty"`
+	Headers              *[]Pair        `json:"headers,omitempty"`
+	Timeout              int64          `json:"timeout"`
 	NotificationInterval int64          `json:"notificationInterval"`
+	Body                 *string        `json:"body,omitempty"`
+	Id                   string         `json:"id"`
+	Type                 MonitorType    `json:"type"`
 	Notifications        []Notification `json:"notifications"`
 	Name                 string         `json:"name"`
-	Type                 MonitorType    `json:"type"`
-	Headers              *[]Pair        `json:"headers,omitempty"`
+	Interval             int64          `json:"interval"`
+	Status               MonitorStatus  `json:"status"`
+	Retries              int64          `json:"retries"`
+	AcceptedStatusCodes  *[]Pair        `json:"acceptedStatusCodes,omitempty"`
 }
-type Pagination struct {
-	Index int64 `json:"index"`
-	Limit int64 `json:"limit"`
-	Total int64 `json:"total"`
+type MonitorList struct {
+	Data       []Monitor  `json:"data"`
+	Pagination Pagination `json:"pagination"`
 }
-type MonitorStatus string
-
-const ACTIVED MonitorStatus = "ACTIVED"
-const DISACTIVED MonitorStatus = "DISACTIVED"
-
 type HttpMethod string
 
 const HEAD HttpMethod = "HEAD"
@@ -124,38 +121,20 @@ type Ordering string
 const ASCENDING Ordering = "ASCENDING"
 const DESCENDING Ordering = "DESCENDING"
 
+type MonitorStatus string
+
+const ACTIVED MonitorStatus = "ACTIVED"
+const DISACTIVED MonitorStatus = "DISACTIVED"
+
 type MonitorApiInterface interface {
-	GetMonitor(gin_context *gin.Context, id string)
-	UpdateMonitor(gin_context *gin.Context, id string, gin_body PutMonitorRequest)
-	DeleteMonitor(gin_context *gin.Context, id string)
 	ListMonitoringRecords(gin_context *gin.Context, id string, index int64, limit int64, startAt int64, endAt int64)
 	CreateMonitor(gin_context *gin.Context, gin_body PutMonitorRequest)
 	ListMonitors(gin_context *gin.Context, ordering Ordering, index int64, limit int64)
+	DeleteMonitor(gin_context *gin.Context, id string)
+	GetMonitor(gin_context *gin.Context, id string)
+	UpdateMonitor(gin_context *gin.Context, id string, gin_body PutMonitorRequest)
 }
 
-func GetMonitorBuilder(api MonitorApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.GetMonitor(gin_context, id)
-	}
-}
-func UpdateMonitorBuilder(api MonitorApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		var putMonitorRequest PutMonitorRequest
-		if err := gin_context.ShouldBindJSON(&putMonitorRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.UpdateMonitor(gin_context, id, putMonitorRequest)
-	}
-}
-func DeleteMonitorBuilder(api MonitorApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.DeleteMonitor(gin_context, id)
-	}
-}
 func ListMonitoringRecordsBuilder(api MonitorApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
 		id := gin_context.Param("id")
@@ -184,13 +163,36 @@ func ListMonitorsBuilder(api MonitorApiInterface) func(c *gin.Context) {
 		api.ListMonitors(gin_context, Ordering(ordering), stringToInt64(index), stringToInt64(limit))
 	}
 }
+func DeleteMonitorBuilder(api MonitorApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.DeleteMonitor(gin_context, id)
+	}
+}
+func GetMonitorBuilder(api MonitorApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.GetMonitor(gin_context, id)
+	}
+}
+func UpdateMonitorBuilder(api MonitorApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		var putMonitorRequest PutMonitorRequest
+		if err := gin_context.ShouldBindJSON(&putMonitorRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.UpdateMonitor(gin_context, id, putMonitorRequest)
+	}
+}
 func MonitorApiInterfaceMounter(gin_router *gin.Engine, gwg_api_label MonitorApiInterface) {
-	gin_router.GET("/monitors/:id", GetMonitorBuilder(gwg_api_label))
-	gin_router.PUT("/monitors/:id", UpdateMonitorBuilder(gwg_api_label))
-	gin_router.DELETE("/monitors/:id", DeleteMonitorBuilder(gwg_api_label))
 	gin_router.GET("/monitors/:id/records", ListMonitoringRecordsBuilder(gwg_api_label))
 	gin_router.POST("/monitors", CreateMonitorBuilder(gwg_api_label))
 	gin_router.GET("/monitors", ListMonitorsBuilder(gwg_api_label))
+	gin_router.DELETE("/monitors/:id", DeleteMonitorBuilder(gwg_api_label))
+	gin_router.GET("/monitors/:id", GetMonitorBuilder(gwg_api_label))
+	gin_router.PUT("/monitors/:id", UpdateMonitorBuilder(gwg_api_label))
 }
 func stringToInt32(s string) int32 {
 	if value, err := strconv.ParseInt(s, 10, 32); err == nil {
